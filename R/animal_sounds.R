808773f7ef77ad89a3345c03a69c3dc73e5811a8
yume <ab02200059@163.com>
2024-06-27 20:06:10 +0800

    others

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,5 @@
-#' Title
-#'
+#' Print What the Animal Says
+#' Print [rlang::abort()]
 #' @param animal a single string
 #' @param sound a single string
 #'
@@ -8,8 +8,24 @@
 #'
 #' @examples
 #' animal_sounds("dogs","woof")
-animal_sounds <- function(animal, sound) {
-  stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
-  paste0("The ", animal, " goes ", sound, "!")
+animal_sounds <- function(animal, sound=NULL) {
+
+    if (!rlang::is_character(animal, 1)) {
+      cli::cli_abort("{.var animal} must be a single string!",
+                     "i"="It was{.type(animal)} of length {length(animal)} instead.",
+                    class="error_not_single_string")
+    }
+    if(is.null(sound))
+  {
+    return (paste0("The ", animal, " makes no sound."))
+  }
+
+    if (!rlang::is_character(sound, 1)) {
+      cli::cli_abort("{.var sound} must be a single string!",
+                     "i"="It was{.type(sound)} of length {length(sound)} instead.",
+                     class="error_not_single_string")
+    }
+
+    paste0("The ", animal, " goes ", sound, "!")
+
 }
